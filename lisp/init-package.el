93f343ae619d3cdb54d10cb4e19a76f2c7a2b3bd
Iven <helei0831@mail.ustc.edu.cn>
2024-03-15 13:03:42 +0800

    download org-roam

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -8,12 +8,10 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;(setq package-archives '(("gnu"   . "http://mirrors.cloud.tencent.com/elpa/gnu/")
-;                         ("melpa" . "http://mirrors.cloud.tencent.com/elpa/melpa/")))
 
 (package-initialize)
 (eval-when-compile
   (require 'use-package))
-(package-refresh-contents)
+;(package-refresh-contents)
 (provide 'init-package)
 ;;; init-package.el ends here
