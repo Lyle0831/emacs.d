86d36651b22e21ce15f29ae4d130a58802c8b040
mac1 <mac1@mac1deMacBook-Pro.local>
2024-03-19 11:53:13 +0800

    finish ivy package

--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -16,18 +16,11 @@
   :config
   ;(setq ivy-use-virtual-buffers t);show some not open buffer, like recent files and bookmark
   (setq ivy-count-format "(%d/%d) ");the format for numbers in minibuffer. "" for no number,"%d" for only the total choice number
-  ;(setq search-default-mode #'char-fold-to-regexp)
+  (setq ivy-extra-directories nil);when find files, no extra file like ./ ../
   (setq ivy-wrap t)         ;make sure in the minibuffer can cycle between the first and last line
   (setq confirm-nonexistent-file-or-buffer t) ;when make a non-exist file or buffer, need an extra RET
   :bind
-  (("C-s" . 'swiper))
-   ;("C-c v" . 'ivy-push-view)
-   ;("C-c s" . 'ivy-switch-view)
-   ;("C-c V" . 'ivy-pop-view)
-   ;("C-x C-@" . 'counsepl-mark-ring);  
+  (("C-s" . 'swiper)))
 
-   ;("C-x C-SPC" . 'counsel-mark-ring)
-   ;:map minibuffer-local-map
-   ;("C-r" . counsel-minibuffer-history)))
-)
+
 (provide 'init-tools)
